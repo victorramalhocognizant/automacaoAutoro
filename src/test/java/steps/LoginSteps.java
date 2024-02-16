9b7e66979aa9f1d64ecc1eac5319a97f679012b2
KarineAugusta <karine.rodrigues@cognizant.com>
2024-02-16 11:05:00 -0300

    resolvendoconflito

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -12,41 +12,52 @@ public class LoginSteps {
     private LoginPage loginPage;
 
     @Given("I open the login Autoropage")
-    public void openAutoroPage(){
+    public void openAutoroPage() {
         loginPage = new LoginPage(Hooks.getDriver());
         loginPage.openSite();
         Hooks.captureScreenshot("pass", "I open the login Autoropage");
     }
-        @When("I put the user name {string}")
+
+    @When("I put the user name {string}")
     public void PutTheUserName(String userName) {
-         loginPage.loginUsuario(userName);
+        loginPage.loginUsuario(userName);
         Hooks.captureScreenshot("pass", "I put the user name");
     }
-        @When("I put the password {string}")
+
+    @When("I put the password {string}")
     public void PutThePassword(String password) {
         loginPage.loginSenha(password);
         Hooks.captureScreenshot("pass", "I put the password");
     }
+
     @When("I click on login button")
     public void ClickOnLoginButton() {
         loginPage.clickLogin();
         Hooks.captureScreenshot("pass", "I click on login button");
     }
+
     @Then("I should see the login page {string}")
     public void ShouldSeeTheLoginPage(String textoValidacao) {
-       Assert.assertEquals(loginPage.getTextHelloJonhSmitString(),textoValidacao);
-       Hooks.captureScreenshot("pass", "I should see the login page");
+        Assert.assertEquals(loginPage.getTextHelloJonhSmitString(), textoValidacao);
+        Hooks.captureScreenshot("pass", "I should see the login page");
     }
 
     @Then("I should see the mensagem {string}")
     public void ShouldSeeTheMensagem(String textoValidacao) {
-       Assert.assertEquals(loginPage.getTextLoginFailed(),textoValidacao);
-       Hooks.captureScreenshot("pass", "I should see the login page");
+        Assert.assertEquals(loginPage.getTextLoginFailed(), textoValidacao);
+        Hooks.captureScreenshot("pass", "I should see the login page");
     }
-    
 
-@Then("I should see the error message {string}")
-public void Errormessage(String textoValidacao) {
-   Assert.assertEquals(loginPage.getTextOnlineBankingLoginString(),textoValidacao);
-   Hooks.captureScreenshot("pass", "I should see the error message");
+    @Then("I should see the error message {string}")
+    public void Errormessage(String textoValidacao) {
+        Assert.assertEquals(loginPage.getTextOnlineBankingLoginString(), textoValidacao);
+        Hooks.captureScreenshot("pass", "I should see the error message");
+    }
+
+    @Then("I should see the pop up {string}")
+    public void ErroPopup(String textoValidacao) {
+        Assert.assertEquals(loginPage.validaPopup(), textoValidacao);
+        Hooks.captureScreenshot("pass", "I should see the pop up");
+
+    }
 }
